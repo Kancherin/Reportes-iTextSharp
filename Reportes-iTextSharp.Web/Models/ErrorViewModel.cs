6d1a93454a7f82b867c243f6ed800b1ee84a651c
David <david_horeb@hotmail.com>
2021-07-21 08:23:59 -0300

    Proyecto Base

---
+++ Reportes-iTextSharp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reportes_iTextSharp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
